862674f15e7941fb485038f0f7c41b91490995fe
asessment <muhammadaryaputra1190@gmail.com>
2024-10-03 16:13:37 +0700

    update arya

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
-    <title>Boostrap Login | Ludiflex</title>
+    <title>TravelEease | Register</title>
 
     <style>
         .alert-container {
@@ -32,15 +32,15 @@
                     <img src="images/1.png" class="img-fluid" style="width: 250px;">
                 </div>
                 <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">TravelEease</p>
-                <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Make Traveling More Easy.</small>
+                <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Jadikan Bepergian Lebih Mudah.</small>
             </div>
 
             <!--------------------------- Right Box ----------------------------->
             <div class="col-md-6 right-box">
                 <div class="row align-items-center">
                     <div class="header-text mb-4">
-                        <h2>Make An Account</h2>
-                        <p>We are happy to have a new User.</p>
+                        <h2>Buat Akun</h2>
+                        <p>Kami senang memiliki Pengguna baru.</p>
                     </div>
 
                     <form action="{{ route('register.save') }}" method="POST">
